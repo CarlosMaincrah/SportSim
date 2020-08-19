fafe87604284a775373673adeb6549b7af434710
Carlos Rodriguez <croc1110@hotmail.com>
2020-08-19 09:50:34 -0500

    Update SportSim.py
    Bug fixes

--- SportSim.py
+++ SportSim.py
@@ -628,7 +628,7 @@ def bask_next():
         Label(window, text=f"{i['team1']} ({r[2]})", font=("Courier", 24)).place(x=0, y=80)
         Label(window, text=f"{i['team2']} ({l[2]})", font=("Courier", 24)).place(x=0, y=155)
         Label(window, text="VS", font=("Courier", 24)).place(x=0, y=120)
-        Button(window, text="Bet in this match", command=fut_bet).place(x=420, y=210)
+        Button(window, text="Bet in this match", command=bask_bet).place(x=420, y=210)
         Button(window, text="Next", command=bask_next).place(x=420, y=110)
         Button(window, text="Back", command=mainpage).place(x=0, y=285)
     else:
@@ -773,13 +773,15 @@ def next_img():
             Button(window, text="Tennis (Coming soon)").place(x=0, y=560, height=60, width=270)
             Canvas(window).place(x=270, y=440, height=180, width=270)
             dumpling = games_played[0]
-            Label(window, text=f"{dumpling['score1']} - {dumpling['score2']}", font=("Courier", 16)).place(x=340, y=455)
-            Label(window, text=f"{dumpling['team1']}", font=("Courier", 10)).place(x=280, y=438)
-            Label(window, text=f"{dumpling['team2']}", font=("Courier", 10)).place(x=400, y=477)
+            Label(window, text="Featured match:", font=("Courier", 10)).place(x=280, y=438)
+            Label(window, text=f"{dumpling['team1']}", font=("Courier", 10)).place(x=280, y=465)
+            Label(window, text="VS", font=("Courier", 10)).place(x=390, y=465)
+            Label(window, text=f"{dumpling['team2']}", font=("Courier", 10)).place(x=415, y=465)
             dumpling = games_played2[0]
-            Label(window, text=f"{dumpling['score1']} - {dumpling['score2']}", font=("Courier", 16)).place(x=340, y=515)
-            Label(window, text=f"{dumpling['team1']}", font=("Courier", 10)).place(x=280, y=498)
-            Label(window, text=f"{dumpling['team2']}", font=("Courier", 10)).place(x=400, y=537)
+            Label(window, text="Featured match:", font=("Courier", 10)).place(x=280, y=498)
+            Label(window, text=f"{dumpling['team1']}", font=("Courier", 10)).place(x=280, y=525)
+            Label(window, text="VS", font=("Courier", 10)).place(x=390, y=525)
+            Label(window, text=f"{dumpling['team2']}", font=("Courier", 10)).place(x=415, y=525)
             Button(window, text="About FRIO MX", command=about_page).place(x=445, y=360)
     else:
         panel = Label(window,width= 540, height= 500)
@@ -817,13 +819,15 @@ def mainpage():
     Button(window, text="Tennis (Coming soon)").place(x=0, y=560, height=60, width=270)
     Canvas(window).place(x=270, y=440, height=180, width=270)
     dumpling = games_played[0]
-    Label(window, text=f"{dumpling['score1']} - {dumpling['score2']}", font=("Courier", 16)).place(x=340, y=455)
-    Label(window, text=f"{dumpling['team1']}", font=("Courier", 10)).place(x=280, y=438)
-    Label(window, text=f"{dumpling['team2']}", font=("Courier", 10)).place(x=400, y=477)
+    Label(window, text="Featured match:", font=("Courier", 10)).place(x=280, y=438)
+    Label(window, text=f"{dumpling['team1']}", font=("Courier", 10)).place(x=280, y=465)
+    Label(window, text="VS", font=("Courier", 10)).place(x=390, y=465)
+    Label(window, text=f"{dumpling['team2']}", font=("Courier", 10)).place(x=415, y=465)
     dumpling = games_played2[0]
-    Label(window, text=f"{dumpling['score1']} - {dumpling['score2']}", font=("Courier", 16)).place(x=340, y=515)
-    Label(window, text=f"{dumpling['team1']}", font=("Courier", 10)).place(x=280, y=498)
-    Label(window, text=f"{dumpling['team2']}", font=("Courier", 10)).place(x=400, y=537)
+    Label(window, text="Featured match:", font=("Courier", 10)).place(x=280, y=498)
+    Label(window, text=f"{dumpling['team1']}", font=("Courier", 10)).place(x=280, y=525)
+    Label(window, text="VS", font=("Courier", 10)).place(x=390, y=525)
+    Label(window, text=f"{dumpling['team2']}", font=("Courier", 10)).place(x=415, y=525)
     Button(window, text="About FRIO MX", command=about_page).place(x=445, y=360)
 
 def check_create_acc():
